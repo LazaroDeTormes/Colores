fdaaee5772f1d9ae20237a2f742ad37d7934ad0f
Alejandro Vázquez González <105195259+LazaroDeTormes@users.noreply.github.com>
2023-05-04 11:09:50 +0200

    borrado

--- app/src/main/java/com/example/colores/lista.java
+++ app/src/main/java/com/example/colores/lista.java
@@ -1,9 +1,15 @@
 package com.example.colores;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -27,9 +33,46 @@ public class lista extends AppCompatActivity {
         lista.setAdapter(adaptador);
 
 
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+
+                registerForContextMenu(view);
+
+
+                return true;
+            }
+        });
 
 
 
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        String elemento = lista.getAdapter().getItem(info.position).toString();
+        menu.setHeaderTitle(elemento);
+        inflater.inflate(R.menu.menubaseitem, menu);
+
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.guardarColorMenu:
+                borrarTupla();
+
+        }
+
+        return true;
+    }
+
+    private void borrarTupla() {
     }
 }
